af39fa4c18748a516b74e4d0cdbef72e0835d2f1
Patrikhoho <phousepian@gmail.com>
2026-02-13 14:08:59 +0100

    channel class
    inte gjort block list eller hur man updaterar till sql

---
+++ Channel.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+
+public class Channel {
+
+    private int channelID;
+    private String channelName;
+    private LocalDateTime createdAt;
+    
+    private List<User> membersList;
+    private List<Message> messageList;
+   // private List<User> blockList; 
+
+    public Channel(int channelID, String channelName) {
+        this.channelID = channelID;
+        this.channelName = channelName;
+        this.createdAt = LocalDateTime.now(); 
+        
+        this.membersList = new ArrayList<>();
+        this.messageList = new ArrayList<>();
+       // this.blockList = new ArrayList<>();
+    }
+
+   
+    public void addUser(User user) {
+        if (!membersList.contains(user)) {
+            membersList.add(user);
+        }
+    }
+
+    public void removeUser(User user) {
+        if (membersList.contains(user)){
+        membersList.remove(user);
+        }else{
+        throw new IllegalArgumentException("User is not in the channel"); 
+    }
+    }
+//updatera sql på något sätt också
+    public void createNewMessage(User sender, String content) {
+        Message msg = new Message(sender.getUsername(), content, "text");
+        messageList.add(msg);
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+
+    public int getChannelID() {
+        return channelID;
+    }
+
+    public List<Message> getMessageList() {
+        return messageList;
+    }
+}
