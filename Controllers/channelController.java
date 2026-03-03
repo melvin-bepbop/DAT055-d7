cb5220747df0d12ce8c58e22555dbd36c435945d
Patrikhoho <phousepian@gmail.com>
2026-03-03 13:28:42 +0100

    update channelcontroller.java

--- Controllers/channelController.java
+++ Controllers/channelController.java
@@ -10,11 +10,26 @@ import Services.ChannelService;
 import Services.MessageService;
 import Views.chatView;
 
+/**
+ * The controller responsible for handling user interactions related to channels.
+ * It acts as the mediator between the user interface (Views), the business logic 
+ * and database operations (Services), and the local state of the application (ClientSession).
+ */
+
 public class channelController {
     private ClientSession session; 
     private ChannelService channelService; 
     private MessageService messageService; 
     private chatView chatView;
+
+/**
+     * Constructs a new channelController with the required dependencies.
+     *
+     * @param session        the current client session containing local state
+     * @param channelService the service handling channel-related database logic
+     * @param messageService the service handling message-related database logic
+     * @param chatView       the GUI component where the chat is displayed
+     */
 
     public channelController(ClientSession session, ChannelService channelService, MessageService messageService, chatView chatView) {
         this.session = session;
@@ -22,6 +37,13 @@ public class channelController {
         this.chatView = chatView;
         this.messageService = messageService;
     }
+
+/**
+     * Creates a new global channel, updates the database, and immediately 
+     * switches the user into this newly created channel.
+     *
+     * @param newChannelName the name of the channel to be created
+     */
 
     public void createNewGlobalChannel(String newChannelName) {
         //Tell the Service to update the Database
@@ -32,6 +54,15 @@ public class channelController {
         session.getAccesibleChannels().getChannels().add(newChan);
         changeChannel(newChan);
     }
+
+    /**
+     * Transitions the user to a different channel.
+     * This method notifies the server of the move, retrieves the message history 
+     * for the new channel, updates the local session memory, and finally refreshes 
+     * the user interface to display the new chat room.
+     *
+     * @param channel the target {@link Channel} the user wants to enter
+     */
 
 public void changeChannel(Channel channel) {
         // Tell the Server/Database that the user moved
@@ -48,6 +79,14 @@ public void changeChannel(Channel channel) {
         // Wipe the old chat off the screen and draw the new history
         chatView.displayMessageHistory(history, session.getActiveUser());
     }
+    
+    /**
+     * Retrieves the list of all channels that the current user has access to.
+     * This data is fetched from the local session state.
+     *
+     * @return an {@link AccesibleChannels} object containing the user's available channels
+     */
+
     public AccesibleChannels GetAllChannels() {
         return session.getAccesibleChannels();
     }
