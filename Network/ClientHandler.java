9a4c10e331638b4e2bd1b2e79ec74043c5a8063b
melvin-bepbop <melvinlarsson2005@gmail.com>
2026-03-03 17:54:48 +0100

    smol

--- Network/ClientHandler.java
+++ Network/ClientHandler.java
@@ -2,34 +2,75 @@ package Network;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
-import Models.message;
+import Models.Message;
 
 
 public class ClientHandler implements Runnable {
     private Socket socket;
+    public static ArrayList<ClientHandler> ClientHandlers = new ArrayList<>();
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
 
     public ClientHandler(Socket socket) {
-        this.socket = socket; 
+        try{
+            this.socket = socket; 
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            ClientHandlers.add(this);
+            broadcastMessage("Connected to the server");
+
+        }catch(IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
     }
 
-    @Override public void run()  {
-        try (
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        ) {
-          
-            while (true) {
-                message message = (message) in.readObject();
-                System.out.println("Received: " + message.getContent());
-
-                // Send response
-                out.writeObject(new message("Server received: " + message.getContent());
-                out.flush();
+    @Override 
+    public void run()  {
+        String messageFromClient;
+        while (socket.isConnected()) {
+            try{
+                messageFromClient = bufferedReader.readLine();
+                
+                broadcastMessage(messageFromClient);
             }
-        } catch (EOFException e) {
-            System.out.println("Client disconnected.");
-        } catch (Exception e) {
+            catch(IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+    public void broadcastMessage(String message){
+        try{
+            for(ClientHandler ch : ClientHandlers){
+            ch.bufferedWriter.write(message);
+            ch.bufferedWriter.newLine();
+            ch.bufferedWriter.flush();
+        }
+        }
+    
+        catch(IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+    public void removeClientHandler(){
+        ClientHandlers.remove(this);
+        broadcastMessage("User has left");
+    }
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        removeClientHandler();
+        try{
+            if(bufferedReader != null){
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
